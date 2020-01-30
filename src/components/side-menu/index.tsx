9332758c35b15292cac2b008e454a56478ded786
Gustavo Taminato <gtaminato@gmail.com>
2020-01-29 22:52:07 -0300

    Update side-menu

--- src/components/side-menu/index.tsx
+++ src/components/side-menu/index.tsx
@@ -16,48 +16,22 @@ class SideMenu extends React.Component {
 
   render() {
     const { Sider } = Layout;
-    const { SubMenu } = Menu;
 
     return (
       <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
         <div className="logo" />
         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
           <Menu.Item key="1">
-            <Icon type="pie-chart" />
-            <span>Option 1</span>
+            <Icon type="heart" />
+            <span>Dashboard</span>
           </Menu.Item>
           <Menu.Item key="2">
-            <Icon type="desktop" />
-            <span>Option 2</span>
+            <Icon type="team" />
+            <span>Teams</span>
           </Menu.Item>
-          <SubMenu
-            key="sub1"
-            title={
-              <span>
-                <Icon type="user" />
-                <span>User</span>
-              </span>
-            }
-          >
-            <Menu.Item key="3">Tom</Menu.Item>
-            <Menu.Item key="4">Bill</Menu.Item>
-            <Menu.Item key="5">Alex</Menu.Item>
-          </SubMenu>
-          <SubMenu
-            key="sub2"
-            title={
-              <span>
-                <Icon type="team" />
-                <span>Team</span>
-              </span>
-            }
-          >
-            <Menu.Item key="6">Team 1</Menu.Item>
-            <Menu.Item key="8">Team 2</Menu.Item>
-          </SubMenu>
-          <Menu.Item key="9">
-            <Icon type="file" />
-            <span>File</span>
+          <Menu.Item key="3">
+            <Icon type="logout" />
+            <span>Logout</span>
           </Menu.Item>
         </Menu>
       </Sider>
