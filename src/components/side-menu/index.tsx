1febc5b22352e222011d9c0b9d6cb71bce47c4d7
Gustavo Taminato <gtaminato@gmail.com>
2020-01-29 23:28:23 -0300

    Update side menu

--- src/components/side-menu/index.tsx
+++ src/components/side-menu/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { Layout, Menu, Icon } from 'antd';
 
-class SideMenu extends React.Component {
+import './styles.scss'
+
+class SideMenu extends React.Component<{
+  picture?: string,
+  name?: string
+}> {
   state = {
     collapsed: false
+  };
+
+  static defaultProps = {
+    picture: 'https://img.ibxk.com.br/2016/10/programas/9167627095224276.png',
+    name: 'Gestor de Pedidos'
   };
 
   onCollapse = () => {
@@ -16,10 +26,21 @@ class SideMenu extends React.Component {
 
   render() {
     const { Sider } = Layout;
+    const { picture, name } = this.props;
 
     return (
-      <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
-        <div className="logo" />
+      <Sider
+        className="SideMenu"
+        collapsible
+        collapsed={this.state.collapsed}
+        onCollapse={this.onCollapse}
+      >
+        <div className="SideMenu__team">
+          <div className="SideMenu__team-logo-wrapper">
+            <img className="SideMenu__team-logo" src={picture} />
+          </div>
+          <h3 className="SideMenu__team-name">{name}</h3>
+        </div>
         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
           <Menu.Item key="1">
             <Icon type="heart" />
