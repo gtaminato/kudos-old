a66ff4b6f74a04df332e98da495580b96e979151
Gustavo Taminato <gtaminato@gmail.com>
2020-10-14 18:46:55 -0300

    Refactoring to use functional component

--- src/components/side-menu/index.tsx
+++ src/components/side-menu/index.tsx
@@ -1,69 +1,50 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
 import { Layout, Menu, Icon } from 'antd';
 
 import './styles.scss'
 
-class SideMenu extends React.Component<{
+const SideMenu: FC<{
   picture?: string,
   name?: string,
-  onLogout: () => void
-}> {
-  state = {
-    collapsed: false
-  };
+  onLogout?: () => void
+}> = ({
+  picture = 'https://img.ibxk.com.br/2016/10/programas/9167627095224276.png',
+  name = 'Gestor de Pedidos',
+  onLogout
+}) => {
+  const [collapsed, setCollapsed] = useState(false);
 
-  static defaultProps = {
-    picture: 'https://img.ibxk.com.br/2016/10/programas/9167627095224276.png',
-    name: 'Gestor de Pedidos'
-  };
+  const { Sider } = Layout;
 
-  onCollapse = () => {
-    const collapsed = this.state.collapsed;
-
-    this.setState({
-      collapsed: !collapsed
-    });
-  }
-
-  handleLogout = () => {
-    const { onLogout } = this.props;
-    onLogout();
-  };
-
-  render() {
-    const { Sider } = Layout;
-    const { picture, name } = this.props;
-
-    return (
-      <Sider
-        className="SideMenu"
-        collapsible
-        collapsed={this.state.collapsed}
-        onCollapse={this.onCollapse}
-      >
-        <div className="SideMenu__team">
-          <div className="SideMenu__team-logo-wrapper">
-            <img className="SideMenu__team-logo" src={picture} alt="iFood" />
-          </div>
-          <h3 className="SideMenu__team-name">{name}</h3>
+  return (
+    <Sider
+      className="SideMenu"
+      collapsible
+      collapsed={collapsed}
+      onCollapse={() => setCollapsed(!collapsed)}
+    >
+      <div className="SideMenu__team">
+        <div className="SideMenu__team-logo-wrapper">
+          <img className="SideMenu__team-logo" src={picture} alt="iFood" />
         </div>
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-          <Menu.Item key="1">
-            <Icon type="heart" />
-            <span>Dashboard</span>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Icon type="team" />
-            <span>Teams</span>
-          </Menu.Item>
-          <Menu.Item key="3" onClick={this.handleLogout}>
-            <Icon type="logout" />
-            <span>Logout</span>
-          </Menu.Item>
-        </Menu>
-      </Sider>
-    );
-  }
+        <h3 className="SideMenu__team-name">{name}</h3>
+      </div>
+      <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+        <Menu.Item key="1">
+          <Icon type="heart" />
+          <span>Dashboard</span>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <Icon type="team" />
+          <span>Teams</span>
+        </Menu.Item>
+        <Menu.Item key="3" onClick={onLogout}>
+          <Icon type="logout" />
+          <span>Logout</span>
+        </Menu.Item>
+      </Menu>
+    </Sider>
+  );
 };
 
 export default SideMenu;
